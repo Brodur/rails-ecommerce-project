81e4785e8cc0038906bb92b21325c7ab1134228f
Brodie Giesbrecht <brodiejgiesbrecht@gmail.com>
2020-11-08 09:52:22 -0600

    Modified Null constraints for Customers and Provinces migrations

--- db/migrate/20201108154042_create_customers.rb
+++ db/migrate/20201108154042_create_customers.rb
@@ -1,10 +1,10 @@
 class CreateCustomers < ActiveRecord::Migration[6.0]
   def change
     create_table :customers do |t|
-      t.string :name
-      t.string :username
-      t.string :email
-      t.string :password
+      t.string :name, null: false
+      t.string :username, null: false
+      t.string :email, null: false
+      t.string :password, null: false
 
       t.timestamps
     end
