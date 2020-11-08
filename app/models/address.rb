cab7042f943790ce26f63bb4e967eff1934dd0a9
Brodie Giesbrecht <brodiejgiesbrecht@gmail.com>
2020-11-08 10:38:17 -0600

    Created address.rb for model

---
+++ app/models/address.rb
@@ -0,0 +1,5 @@
+class Address < ApplicationRecord
+  belongs_to :customer
+  belongs_to :province
+  has_many :orders
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,3 @@
 class Product < ApplicationRecord
+  has_many :orders, through: :products_orders
 end
