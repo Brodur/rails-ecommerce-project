63755acfcc0efc7cc3f61bd1dfa2fe39e79f2d86
Brodie Giesbrecht <brodiejgiesbrecht@gmail.com>
2020-11-08 10:13:22 -0600

    Generated Orders model

---
+++ db/migrate/20201108161253_create_orders.rb
@@ -0,0 +1,14 @@
+class CreateOrders < ActiveRecord::Migration[6.0]
+  def change
+    create_table :orders do |t|
+      t.references :customers, null: false, foreign_key: true
+      t.references :address, null: false, foreign_key: true
+      t.datetime :order_date
+      t.decimal :historic_pst_rate
+      t.decimal :historic_gst_rate
+      t.decimal :historic_hst_rate
+
+      t.timestamps
+    end
+  end
+end
