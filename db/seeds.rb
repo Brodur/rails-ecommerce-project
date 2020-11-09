df446585288dc03a009b738a1b3af27c3834608f
Brodie Giesbrecht <brodiejgiesbrecht@gmail.com>
2020-11-09 10:40:30 -0600

    Seeded Provinces

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,97 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+# Delete ALL block
+ProductsOrder.delete_all
+Category.delete_all
+Product.delete_all
+Order.delete_all
+Address.delete_all
+Province.delete_all
+Customer.delete_all
+
+# Seed block
+## Provinces
+Province.create(
+  name:     "Alberta",
+  code:     "AB",
+  gst_rate: 0.05
+)
+
+Province.create(
+  name:     "British Columbia",
+  code:     "BC",
+  gst_rate: 0.05,
+  pst_rate: 0.07
+)
+
+Province.create(
+  name:     "Manitoba",
+  code:     "MB",
+  gst_rate: 0.05,
+  pst_rate: 0.07
+)
+
+Province.create(
+  name:     "New Brunswick",
+  code:     "NB",
+  hst_rate: 0.15
+)
+
+Province.create(
+  name:     "Newfoundland and Labrador",
+  code:     "NL",
+  hst_rate: 0.15
+)
+
+Province.create(
+  name:     "Nova Scotia",
+  code:     "NL",
+  hst_rate: 0.15
+)
+
+Province.create(
+  name:     "Northwest Territories",
+  code:     "NT",
+  gst_rate: 0.05
+)
+
+Province.create(
+  name:     "Nunavut",
+  code:     "NU",
+  gst_rate: 0.05
+)
+
+Province.create(
+  name:     "Ontario",
+  code:     "ON",
+  hst_rate: 0.13
+)
+
+Province.create(
+  name:     "Prince Edward Island",
+  code:     "PE",
+  hst_rate: 0.15
+)
+
+Province.create(
+  name:     "Quebec",
+  code:     "QC",
+  gst_rate: 0.05,
+  pst_rate: 0.09975
+)
+
+Province.create(
+  name:     "Saskatchewan",
+  code:     "SK",
+  gst_rate: 0.05,
+  pst_rate: 0.06
+)
+
+Province.create(
+  name:     "Yukon",
+  code:     "YT",
+  gst_rate: 0.05
+)
+
+## Active Admin
+# if Rails.env.development?
+#   AdminUser.create!(email: "admin@example.com", password: "password", password_confirmation: "password")
+# end
