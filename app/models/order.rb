6cb26364ae6514c173af9066aadf4abf95f910e0
Brodie Giesbrecht <brodiejgiesbrecht@gmail.com>
2020-11-09 10:23:34 -0600

    Fixed has_many through associations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :customer
   belongs_to :address
+  has_many :products_orders
   has_many :products, through: :products_orders
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
   belongs_to :category
+  has_many :products_orders
   has_many :orders, through: :products_orders
 end
