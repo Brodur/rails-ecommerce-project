b3f588fcb1ca0330d2589611365639741575487c
Brodie Giesbrecht <brodiejgiesbrecht@gmail.com>
2020-11-09 09:07:56 -0600

    Updated AR Associations for Categories and Products

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ApplicationRecord
   has_many :children, class_name: "Category", foreign_key: "parent_id"
+  has_many :products
   belongs_to :parent, class_name: "Category", optional: true
 end
