898caf83f5fcb58ecc90a817a93ab0bfbbe54456
Brodie Giesbrecht <brodiejgiesbrecht@gmail.com>
2020-11-10 11:09:58 -0600

    Category Validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,21 @@ class Category < ApplicationRecord
   has_many :children, class_name: "Category", foreign_key: "parent_id"
   has_many :products
   belongs_to :parent, class_name: "Category", optional: true
+
+  validates :name, presence: true
+  # validates_with CategoryValidator, fields: [:parent]
+
+  # def self.path
+  #   path = []
+  #   parent = self.parent
+  #   until parent.empty?
+  #     path << parent
+  #     parent = parent.parent
+  #   end
+  #   path.reverse
+  # end
 end
+
+# class CategoryValidator < ActiveModel::Validator
+#   def validate(record); end
+# end
