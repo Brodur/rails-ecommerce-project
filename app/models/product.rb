f53762b7caa1c281c4fc67e059be4122ae74e2cb
Brodie Giesbrecht <brodiejgiesbrecht@gmail.com>
2020-11-10 10:46:42 -0600

    Products Validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,8 @@ class Product < ApplicationRecord
   belongs_to :category
   has_many :products_orders
   has_many :orders, through: :products_orders
+
+  validates :upc, :name, :price, :cost, presence: true
+  validates :upc, numericality: true, length: { is: 12 }
+  validates :price, :cost, numericality: true
 end
