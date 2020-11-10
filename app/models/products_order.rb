3c67b52a517dc35e3571589352728c4b8405a246
Brodie Giesbrecht <brodiejgiesbrecht@gmail.com>
2020-11-10 10:43:13 -0600

    ProductsOrders validations

--- app/models/products_order.rb
+++ app/models/products_order.rb
@@ -1,4 +1,7 @@
 class ProductsOrder < ApplicationRecord
   belongs_to :order
   belongs_to :product
+
+  validates :historic_cost, :historic_price, :quantity, numericality: true
+  validates :historic_cost, :historic_price, presence: true
 end
