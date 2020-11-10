3e83633850be574dcef9b1d158866a08c194f69e
Brodie Giesbrecht <brodiejgiesbrecht@gmail.com>
2020-11-10 09:33:39 -0600

    Validations for addresses

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,7 @@ class Address < ApplicationRecord
   belongs_to :customer
   belongs_to :province
   has_many :orders
+  validates :postal_code, :country_code, :city, :address_line_one, presence: true
+  validates :postal_code, length: { maximum: 7 }
+  validates :country_code, length: { is: 2 }
 end
