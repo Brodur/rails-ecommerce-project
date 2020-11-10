733fe19556c7c961d4d89bb7b673c36020871298
Brodie Giesbrecht <brodiejgiesbrecht@gmail.com>
2020-11-10 09:36:05 -0600

    Customer Validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,6 @@ class Customer < ApplicationRecord
   has_many :addresses
   has_many :orders
   has_many :provinces, through: :addresses
+  validates :name, :username, :email, :password, presence: true
+  validates :password, length: { minimum: 10 }
 end
