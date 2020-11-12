c14d867fa2deb7aecef5cd2cfebb881d120cc737
Brodie Giesbrecht <brodiejgiesbrecht@gmail.com>
2020-11-12 14:19:09 -0600

    Moved before_actions back to products controller so that the only constraint would apply.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,23 @@
 class ProductsController < ApplicationController
+  before_action :initialize_session
+  before_action :increment_visit_count, only[:index, :show]
+
   def index
     @products = Product.all.order("RANDOM()").limit(6).includes(:category)
   end
 
   def show
     @product = Product.find(params[:id])
+  end
+
+  private
+
+  def initialize_session
+    session[:visit_count] ||= 0
+  end
+
+  def increment_visit_count
+    session[:visit_count] += 1
+    @visit_count = session[:visit_count]
   end
 end
