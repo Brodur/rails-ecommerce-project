731b753c0f9be3bc082640f665e524723d7759f3
Brodie Giesbrecht <brodiejgiesbrecht@gmail.com>
2020-11-12 14:31:14 -0600

    Added session init to products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,17 @@
 class ProductsController < ApplicationController
+  before_action :initialize_session
+
   def index
-    @products = Product.all.order("RANDOM()").limit(6).includes(:category)
+    @products = Product.all.limit(6).includes(:category)
   end
 
   def show
     @product = Product.find(params[:id])
+  end
+
+  private
+
+  def initialize_session
+    session[:visit_count] ||= 0
   end
 end
