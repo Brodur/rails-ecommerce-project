4a8cd4349f77f5a66766043ed6bdea78b8c0227c
Brodie Giesbrecht <brodiejgiesbrecht@gmail.com>
2020-11-12 10:20:17 -0600

    Basic show and index for Categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   def index
-    @categories = Category.all
+    @categories = Category.all.includes(:products, :children, :parent)
   end
 
   def show
--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,7 @@
 module CategoriesHelper
+  def path_to_string(category)
+    path = category.path.map { |category| link_to category.name, category }.join("/")
+    path += "/" + (link_to category.name, category)
+    path.html_safe
+  end
 end
