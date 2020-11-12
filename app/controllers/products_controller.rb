b7af047e39c0cf73a48c11125627aac7a92faa2b
Brodie Giesbrecht <brodiejgiesbrecht@gmail.com>
2020-11-12 14:12:57 -0600

    Refactored code to private methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,20 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all.order("RANDOM()").limit(6).includes(:category)
-    session[:visit_count] ||= 0
-    session[:visit_count] += 1
-    @visit_count = session[:visit_count]
   end
 
   def show
     @product = Product.find(params[:id])
+  end
+
+  private
+
+  def initialize_session
+    session[:visit_count] ||= 0
+  end
+
+  def incrememnt_visit_count
+    session[:visit_count] += 1
+    @visit_count = session[:visit_count]
   end
 end
