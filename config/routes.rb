8a143fb1a44f55d5ff38a07a121b105c6472a027
Brodie Giesbrecht <brodiejgiesbrecht@gmail.com>
2020-11-12 09:53:06 -0600

    Fixed controller names

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'product/index'
-  get 'product/show'
-  get 'category/index'
-  get 'category/show'
+  resource :products, only: %i[index show]
+  resource :categories, only: %i[index show]
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
