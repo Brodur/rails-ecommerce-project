1c9236cdff8396dfc48e0f453cb2157cb4d52bd8
Brodie Giesbrecht <brodiejgiesbrecht@gmail.com>
2020-11-12 14:03:10 -0600

    Implemented simple visit counter following along with rails session video

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,9 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all.order("RANDOM()").limit(6).includes(:category)
+    session[:visit_count] ||= 0
+    session[:visit_count] += 1
+    @visit_count = session[:visit_count]
   end
 
   def show
