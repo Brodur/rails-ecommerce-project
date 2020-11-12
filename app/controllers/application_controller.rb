32a5b147282a67d4993d727e44c621e00f4a985c
Brodie Giesbrecht <brodiejgiesbrecht@gmail.com>
2020-11-12 14:17:01 -0600

    Moved before actions to the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :initialize_session
+  before_action :increment_visit_count
+
+  private
+
+  def initialize_session
+    session[:visit_count] ||= 0
+  end
+
+  def increment_visit_count
+    session[:visit_count] += 1
+    @visit_count = session[:visit_count]
+  end
 end
