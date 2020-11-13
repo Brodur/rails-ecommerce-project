298494c21acf3d30002c328100f30ab8498f23e7
Brodie Giesbrecht <brodiejgiesbrecht@gmail.com>
2020-11-13 15:45:34 -0600

    Removed turbo links

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,22 +1,22 @@
 class ProductsController < ApplicationController
   def index
-    products = Product.all.limit(6).includes(:category)
-    render inertia: "Product/Index",
-           props:   {
-             products: products.as_json(
-               only: %i[id upc name price cost description on_hand category]
-             )
-           }
+    @products = Product.all.limit(6).includes(:category)
+    # render inertia: "Product/Index",
+    #        props:   {
+    #          products: products.as_json(
+    #            only: %i[id upc name price cost description on_hand category]
+    #          )
+    #        }
   end
 
   def show
-    product = Product.find(params[:id])
-    render inertia: "Product/Show",
-           props:   {
-             product: product.as_json(
-               only: %i[id upc name price cost description on_hand category]
-             )
-           }
+    @product = Product.find(params[:id])
+    # render inertia: "Product/Show",
+    #        props:   {
+    #          product: product.as_json(
+    #            only: %i[id upc name price cost description on_hand category]
+    #          )
+    #        }
   end
 
   def add_to_cart
