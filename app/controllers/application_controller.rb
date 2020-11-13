dfbe59f9f46d77c1cb5c89977eb530da6738f8bb
Brodie Giesbrecht <brodiejgiesbrecht@gmail.com>
2020-11-13 13:16:12 -0600

    Added cart loading to all pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :initialize_session
+  before_action :load_cart
+
+  private
+
+  def initialize_session
+    session[:cart] ||= {}
+  end
+
+  def load_cart
+    @cart = session[:cart].map { |id, qty| [Product.find(id.to_i), qty] }.to_h
+  end
 end
