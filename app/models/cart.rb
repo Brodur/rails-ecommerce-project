3bcc9d80935e9852a8ca0885b849c10132576d22
Brodie Giesbrecht <brodiejgiesbrecht@gmail.com>
2020-11-17 13:58:14 -0600

    More cart validations

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,5 @@ class Cart < ApplicationRecord
   belongs_to :product
 
   validates :session_id, :product, :quantity, presence: true
+  validates :quantity, numericality: { only_integer: true }
 end
