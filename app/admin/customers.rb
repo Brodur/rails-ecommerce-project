b43b2b7d5eebe33714e4427a79f9538b2746ace5
Brodie Giesbrecht <brodiejgiesbrecht@gmail.com>
2020-11-27 13:12:51 -0600

    Fixed customers for active admin

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,12 +1,28 @@
 ActiveAdmin.register Customer do
-  permit_params :name, :username, :email, :password
+  permit_params :name, :email, :password, :password_confirmation
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :email
+    column :current_sign_in_at
+    column :sign_in_count
+    column :created_at
+    actions
+  end
+
+  filter :email
+  filter :current_sign_in_at
+  filter :sign_in_count
+  filter :created_at
 
   form do |f|
-    f.inputs "Details" do
+    f.inputs do
       f.input :name
-      f.input :username
       f.input :email
-      f.input :password, as: :string
+      f.input :password
+      f.input :password_confirmation
     end
     f.actions
   end
