0e1f637612760530ff40d66efe0e9ea9b91113b6
Brodie Giesbrecht <brodiejgiesbrecht@gmail.com>
2020-11-27 11:10:48 -0600

    Recreated customers table with devise

---
+++ db/migrate/20201127170641_add_customers.rb
@@ -0,0 +1,9 @@
+class AddCustomers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :customers do |t|
+      t.string :name, null: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_27_170422) do
+ActiveRecord::Schema.define(version: 2020_11_27_170642) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -91,6 +91,19 @@ ActiveRecord::Schema.define(version: 2020_11_27_170422) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["parent_id"], name: "index_categories_on_parent_id"
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_customers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
   create_table "orders", force: :cascade do |t|
