6d2712120a324b3b238225b37bb1d2476e4a953d
Brodie Giesbrecht <brodiejgiesbrecht@gmail.com>
2020-11-27 11:23:15 -0600

    Updated validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,7 +6,6 @@ class Customer < ApplicationRecord
   has_many :addresses
   has_many :orders
   has_many :provinces, through: :addresses
-  validates :name, :username, :email, :password, presence: true
-  validates :password, length: { minimum: 10 }
+  validates :name, :email, presence: true
   validates :email, uniqueness: true
 end
