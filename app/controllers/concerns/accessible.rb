ad7e768e250a54655829dfadef423db9157327ae
Brodie Giesbrecht <brodiejgiesbrecht@gmail.com>
2020-11-27 13:03:10 -0600

    Enabled scoped views

--- app/controllers/concerns/accessible.rb
+++ app/controllers/concerns/accessible.rb
@@ -11,7 +11,7 @@ module Accessible
       flash.clear
       # if you have rails_admin. You can redirect anywhere really
       redirect_to(admin_dashboard) and return
-    elsif current_user
+    elsif current_customer
       flash.clear
       # The authenticated root path can be defined in your routes.rb in: devise_scope :user do...
       redirect_to(root_path) and return
