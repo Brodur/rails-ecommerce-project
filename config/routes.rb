3b66baae548dd38a04c3f7fd82161bbe38989120
Brodie Giesbrecht <brodiejgiesbrecht@gmail.com>
2020-11-28 13:31:28 -0600

    scaffolded addresses controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :products, only: %i[index show]
   resources :categories, only: %i[index show]
   resources :front_page, only: %i[index]
+  resources :addresses
 
   post "products/add_to_cart/:id", to: "products#add_to_cart", as: "add_to_cart"
   delete "products/remove_from_cart/:id", to: "products#remove_from_cart", as: "remove_from_cart"
